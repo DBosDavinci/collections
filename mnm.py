30a158e4cc7fd4aefac55ed4b14acfb96f892182
DBosDavinci <99069008@mydavinci.nl>
2021-10-01 11:36:56 +0200

    sorteren

--- mnm.py
+++ mnm.py
@@ -1,22 +1,43 @@
 from random import randrange
+from typing import List
 
 kleuren = ["oranje","blauw","groen","bruin"]
+zakList = []
+zakDictionary = {"oranje":0,"blauw":0,"groen":0,"bruin":0}
+hoeveel = int(input("Hoeveel kleuren moeten er aan de zak m&m's toegevoegd worden?"))
 
 def mnmFunctieList(hoeveel:int):
-    zakList = []
     for x in range(1,(hoeveel+1)):
         zakList.append(kleuren[randrange(0,4)])
         x+=1
     print(zakList)
 
 def mnmFunctieDict(hoeveel:int):
-    zakDictionary = {"oranje":0,"blauw":0,"groen":0,"bruin":0}
     for x in range(1,(hoeveel+1)):
         kleur = kleuren[randrange(0,4)]
         zakDictionary[kleur] = zakDictionary[kleur] + 1
         x+=1
     print(zakDictionary)
 
-hoeveel = int(input("Hoeveel kleuren moeten er aan de zak m&m's toegevoegd worden?"))
+def mnmFunctieSorteer(SorteerList:list,SorteerDict:dict):
+    global hoeveel
+    o = SorteerDict.get("oranje")
+    bl = SorteerDict.get("blauw")
+    g = SorteerDict.get("groen")
+    br = SorteerDict.get("bruin")
+    x = 1
+    while x < hoeveel:
+        if SorteerList[x] == "oranje":
+            o = o + 1
+        elif SorteerList[x] == "blauw":
+            bl = bl + 1
+        elif SorteerList[x] == "groen":
+            g = g + 1
+        elif SorteerList[x] == "bruin":
+            br = br + 1
+        x = x + 1
+    print("Er zijn {} oranje, {} blauwe, {} groene en {} bruine m&m's".format(o,bl,g,br))
+
 mnmFunctieList(hoeveel)
 mnmFunctieDict(hoeveel)
+mnmFunctieSorteer(zakList,zakDictionary)
