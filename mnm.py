d16919fa20cf953283f04afe70901702a2380f55
DBosDavinci <99069008@mydavinci.nl>
2021-10-01 10:27:26 +0200

    mnmDict

--- mnm.py
+++ mnm.py
@@ -1,14 +1,22 @@
 from random import randrange
 
 kleuren = ["oranje","blauw","groen","bruin"]
-zakList = []
 
-def mnmFunctie(hoeveel:int):
-    global zakList
+def mnmFunctieList(hoeveel:int):
+    zakList = []
     for x in range(1,(hoeveel+1)):
-        zakList.append(kleuren[randrange(0,3)])
+        zakList.append(kleuren[randrange(0,4)])
         x+=1
     print(zakList)
 
+def mnmFunctieDict(hoeveel:int):
+    zakDictionary = {"oranje":0,"blauw":0,"groen":0,"bruin":0}
+    for x in range(1,(hoeveel+1)):
+        kleur = kleuren[randrange(0,4)]
+        zakDictionary[kleur] = zakDictionary[kleur] + 1
+        x+=1
+    print(zakDictionary)
+
 hoeveel = int(input("Hoeveel kleuren moeten er aan de zak m&m's toegevoegd worden?"))
-mnmFunctie(hoeveel)
+mnmFunctieList(hoeveel)
+mnmFunctieDict(hoeveel)
